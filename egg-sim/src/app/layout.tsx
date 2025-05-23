e644dca2591db7e7d9ef3859a4a99637dc6d5ded
Ivan Wang <ivanwang1239@gmail.com>
2025-05-22 21:05:56 -0400

    set up basic layout for about page

--- egg-sim/src/app/layout.tsx
+++ egg-sim/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import NavBar from "./components/navBar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,9 +26,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col h-screen`}
       >
-        {children}
+        <NavBar></NavBar>
+        <main className="flex-1 overflow-auto mt-[5rem]">
+          {children}
+        </main>
       </body>
     </html>
   );
