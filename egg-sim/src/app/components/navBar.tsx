9af9a645b8b0d4df1eead5a447cb0d5d557f1a3d
Evan Lin <evanl3375@gmail.com>
2025-05-23 21:00:07 -0400

    hen walking from nest, no chick support.

--- egg-sim/src/app/components/navBar.tsx
+++ egg-sim/src/app/components/navBar.tsx
@@ -2,18 +2,18 @@ import NavButton from "./navButton";
 
 export default function NavBar() {
   return (
-    <nav className="w-full h-[5rem] flex flex-col bg-red-400 top-0 fixed">
+    <div className="w-full h-[5rem] flex flex-col bg-[#C89F77] top-0 fixed">
       <div className="w-full h-full flex flex-row items-center pl-20">
         <p className="w-full font-bold text-2xl text-white">
           EggSim
         </p>
         <div className="w-full flex flex-row">
-          <NavButton name="Home" route=""></NavButton>
+          <NavButton name="Home" route="home"></NavButton>
           <NavButton name="About" route="about"></NavButton>
           <NavButton name="FAQ" route="faq"></NavButton>
           <NavButton name="Contact Us" route=""></NavButton>
         </div>
       </div>
-    </nav>
+    </div>
   );
 }
