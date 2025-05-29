feca35728dd2cde9aad5ac5691a67472edf9907b
Ivan Wang <ivanwang1239@gmail.com>
2025-05-28 20:18:44 -0400

    create inventory button and feature

--- egg-sim/src/app/page.tsx
+++ egg-sim/src/app/page.tsx
@@ -1,10 +1,20 @@
 'use client'
+import { useState } from "react";
+import Inventory from "./components/inventory";
 
 export default function Home() {
+
+  const [openInventory, setOpenInventory] = useState(false);
 
   return (
     <div className="w-full h-full flex flex-col bg-amber-200">
       hello
+      <button className="w-[5rem] h-[5rem] bg-red-300" onClick={() => setOpenInventory(true)}>
+        Inventory
+      </button>
+      {openInventory && (
+        <Inventory onClose={() => setOpenInventory(false)}></Inventory>
+      )}
     </div>
   );
 }
