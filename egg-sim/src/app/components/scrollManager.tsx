3962f3d6fb6fb86186dd1b7bc3a42b69486b4b61
Evan Lin <evanl3375@gmail.com>
2025-06-14 19:56:28 -0400

    finished basic vision of boil page. further updates will just be to improve upon this version

--- egg-sim/src/app/components/scrollManager.tsx
+++ egg-sim/src/app/components/scrollManager.tsx
@@ -7,14 +7,14 @@ export default function ScrollManager() {
   const pathname = usePathname();
 
   // list of paths where scrolling should be disabled
-  const noScrollPages = ["/home", "/fry"]; 
+  const noScrollPages = ["/home", "/fry", "/boil"];
   const isNoScrollPage = noScrollPages.includes(pathname);
 
   useEffect(() => {
     if (isNoScrollPage) {
       document.documentElement.style.overflow = "hidden"; // d\isables both vertical and horizontal scroll
     } else {
-      document.documentElement.style.overflow = ""; 
+      document.documentElement.style.overflow = "";
     }
   }, [isNoScrollPage]);
 
